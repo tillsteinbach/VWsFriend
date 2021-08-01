4f6355779361fc42b6a522c098f186cc9bcb36bb
Till Steinbach <steinbach.till@gmx.de>
2021-08-01 21:57:47 +0200

    fixes

---
+++ vwsfriend/vwsfriend/agents/climatization_agent.py
@@ -0,0 +1,35 @@
+from vwsfriend.model.climatization import Climatization
+
+from weconnect.addressable import AddressableLeaf
+
+
+class ClimatizationAgent():
+    def __init__(self, session, vehicle):
+        self.session = session
+        self.vehicle = vehicle
+        self.charge = session.query(Climatization).filter(Climatization.vehicle == vehicle).order_by(Climatization.carCapturedTimestamp.desc()).first()
+
+        # register for updates:
+        if self.vehicle.weConnectVehicle is not None:
+            if 'climatisationStatus' in self.vehicle.weConnectVehicle.statuses and self.vehicle.weConnectVehicle.statuses['climatisationStatus'].enabled:
+                self.vehicle.weConnectVehicle.statuses['climatisationStatus'].carCapturedTimestamp.addObserver(self.__onCarCapturedTimestampChange, AddressableLeaf.ObserverEvent.VALUE_CHANGED, onUpdateComplete=True)
+                self.__onCarCapturedTimestampChange(None, None)
+
+    def __onCarCapturedTimestampChange(self, element, flags):
+        chargeStatus = self.vehicle.weConnectVehicle.statuses['climatisationStatus']
+        current_remainingClimatisationTime_min = None
+        current_climatisationState = None
+        if chargeStatus.remainingClimatisationTime_min.enabled:
+            current_remainingClimatisationTime_min = chargeStatus.remainingClimatisationTime_min.value
+        if chargeStatus.climatisationState.enabled:
+            current_climatisationState = chargeStatus.climatisationState.value
+
+        if self.charge is None or (self.charge.carCapturedTimestamp != chargeStatus.carCapturedTimestamp.value and (
+                self.charge.remainingClimatisationTime_min != current_remainingClimatisationTime_min
+                or self.charge.climatisationState != current_climatisationState)):
+
+            self.charge = Climatization(self.vehicle, chargeStatus.carCapturedTimestamp.value, current_remainingClimatisationTime_min, current_climatisationState)
+            self.session.add(self.charge)
+
+    def commit(self):
+        pass
---
+++ vwsfriend/vwsfriend/model/charging_session.py
@@ -0,0 +1,44 @@
+import enum
+
+from sqlalchemy import Column, Integer, String, DateTime, Enum, ForeignKey, UniqueConstraint
+from sqlalchemy.orm import relationship
+
+from vwsfriend.model.base import Base
+
+from weconnect.elements.charging_settings import ChargingSettings
+
+
+class ACDC(enum.Enum,):
+    AC = enum.auto()
+    DC = enum.auto()
+    UNKNOWN = enum.auto()
+
+
+class ChargingSession(Base):
+    __tablename__ = 'charging_sessions'
+    id = Column(Integer, primary_key=True)
+    vehicle_vin = Column(String, ForeignKey('vehicles.vin'))
+    vehicle = relationship("Vehicle")
+
+    connected = Column(DateTime)
+    locked = Column(DateTime)
+    started = Column(DateTime)
+    ended = Column(DateTime)
+    unlocked = Column(DateTime)
+    disconnected = Column(DateTime)
+    maxChargeCurrentACSetting = Column(Enum(ChargingSettings.MaximumChargeCurrent, length=63))
+    targetSOCSetting_pct = Column(Integer)
+    targetSOCSetting_pct = Column(Integer)
+    maximumChargePower_kW = Column(Integer)
+    averageChargePower_kW = Column(Integer)
+    maximumChargeRate_kmph = Column(Integer)
+    acdc = Column(Enum(ACDC, length=63))
+    startSOC_pct = Column(Integer)
+    endSOC_pct = Column(Integer)
+    mileage_km = Column(Integer)
+    cost = Column(Integer)
+    # To be added Location
+    # To be added Charger
+
+    def __init__(self, vehicle):
+        self.vehicle = vehicle
