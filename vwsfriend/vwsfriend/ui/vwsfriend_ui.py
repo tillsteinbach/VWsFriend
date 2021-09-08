953240dd5e47c238084b95b6a858f651097f101c
Christian Donner <cdonner@podconsulting.com>
2021-09-08 17:02:16 +0200

    version information

--- vwsfriend/vwsfriend/ui/vwsfriend_ui.py
+++ vwsfriend/vwsfriend/ui/vwsfriend_ui.py
@@ -4,6 +4,8 @@ import uuid
 import logging
 import flask
 
+import sqlalchemy
+
 from flask_wtf.csrf import CSRFProtect
 from flask_sqlalchemy import SQLAlchemy
 
@@ -17,6 +19,9 @@ import vwsfriend.ui.status as status
 import vwsfriend.ui.settings as settings
 import vwsfriend.ui.database as database
 
+from vwsfriend.__version import __version__ as __vwsfriend_version__
+from weconnect.__version import __version__ as __weconnect_version__
+
 LOG_LEVELS = ["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"]
 DEFAULT_LOG_LEVEL = "ERROR"
 
@@ -26,7 +31,7 @@ csrf = CSRFProtect()
 
 
 class VWsFriendUI:
-    def __init__(self, weConnect=None, connector=None, homekitDriver=None, dbUrl=None):
+    def __init__(self, weConnect=None, connector=None, homekitDriver=None, dbUrl=None):  # noqa: C901
         self.app = flask.Flask('VWsFriend', template_folder=os.path.dirname(__file__) + '/templates', static_folder=os.path.dirname(__file__) + '/static')
         self.app.debug = True
         self.app.config.from_mapping(
@@ -60,6 +65,9 @@ class VWsFriendUI:
 
         @self.app.before_request
         def before_request_callback():
+            flask.g.versions = dict()
+            flask.g.versions['VWsFriend'] = __vwsfriend_version__
+            flask.g.versions['WeConnect Python Library'] = __weconnect_version__
             if self.app.connector.withDB:
                 flask.g.dbsettings = self.app.db.session.query(Settings).first()
 
@@ -68,6 +76,26 @@ class VWsFriendUI:
             if flask.current_app.connector.withDB and flask.g.dbsettings is None:
                 return flask.redirect(flask.url_for('database.settingsEdit'))
             return flask.redirect(flask.url_for('status.vehicles'))
+
+        @self.app.route('/versions', methods=['GET'])
+        def versions():
+            if flask.current_app.connector.withDB:
+                result = self.app.db.session.execute('SELECT version_num FROM alembic_version LIMIT 1;').fetchone()
+                if result is not None and result:
+                    flask.g.versions['Database Schema'] = result[0]
+                else:
+                    flask.g.versions['Database Schema'] = 'unknown'
+
+                try:
+                    result = self.app.db.session.execute('SELECT version();').fetchone()
+                    if result is not None and result:
+                        flask.g.versions['Database'] = result[0]
+                    else:
+                        flask.g.versions['Database'] = 'unknown'
+                except sqlalchemy.exc.OperationalError:
+                    pass
+
+            return flask.render_template('versions.html', current_app=flask.current_app)
 
         @self.app.route('/healthcheck', methods=['GET'])
         def healthcheck():
