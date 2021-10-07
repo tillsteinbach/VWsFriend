b1140e9a60a16beca3a5146cf69efd45d2a94308
Christian Donner <cdonner@podconsulting.com>
2021-10-07 08:44:26 +0200

    rename base

--- vwsfriend/setup.py
+++ vwsfriend/setup.py
@@ -28,7 +28,7 @@ setup(
     install_requires=INSTALL_REQUIRED,
     entry_points={
         'console_scripts': [
-            'vwsfriend = vwsfriend.vwsfriend:main',
+            'vwsfriend = vwsfriend.vwsfriend_base:main',
         ],
     },
     classifiers=[
