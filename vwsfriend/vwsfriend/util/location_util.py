c46716c8cdb00e3679c2dc7744d7d08af687696a
Till Steinbach <steinbach.till@gmx.de>
2022-02-08 17:47:10 +0100

    try to find actual fuel station when refueling

--- vwsfriend/vwsfriend/util/location_util.py
+++ vwsfriend/vwsfriend/util/location_util.py
@@ -1,5 +1,5 @@
 import requests
-from haversine import haversine, Unit
+from haversine import haversine, Unit, inverse_haversine, Direction
 from sqlalchemy import and_
 
 from vwsfriend.model.geofence import Geofence
@@ -10,6 +10,8 @@ from weconnect.errors import RetrievalError
 
 
 def locationFromLatLonWithGeofence(session, latitude, longitude):
+    if latitude is None or longitude is None:
+        return None
     geofences: Geofence = session.query(Geofence).filter(and_(Geofence.latitude.isnot(None), Geofence.longitude.isnot(None))).all()
     geofenceDistance = [(haversine((latitude, longitude), (geofence.latitude, geofence.longitude), unit=Unit.METERS), geofence) for geofence in geofences]
     geofenceDistance = sorted(geofenceDistance, key=lambda geofence: geofence[0])
@@ -36,13 +38,38 @@ def locationFromLatLon(session, latitude, longitude):
         return session.merge(location)
     return None
 
+def amenityFromLatLon(session, latitude, longitude, radius, amenity, withFallback=False):
+    northWest = inverse_haversine((latitude, longitude), radius, Direction.NORTHWEST, unit=Unit.METERS)
+    southEast = inverse_haversine((latitude, longitude), radius, Direction.SOUTHEAST, unit=Unit.METERS)
+    query = {
+        'q': f'[{amenity}]',
+        'viewbox': f'{northWest[1]},{northWest[0]},{southEast[1]},{southEast[0]}',
+        'bounded': 1,
+        'namedetails': 1,
+        'addressdetails': 1,
+        'format': 'json'
+    }
+    headers = {
+        'User-Agent': 'VWsFriend'
+    }
+    response = requests.get('https://nominatim.openstreetmap.org/search', params=query, headers=headers)
+    if response.status_code == requests.codes['ok']:
+        places = response.json()
+        placesDistance = [(haversine((latitude, longitude), (float(place['lat']), float(place['lon'])), unit=Unit.METERS), place) for place in places]
+        placesDistance = sorted(placesDistance, key=lambda geofence: geofence[0])
+        for distance, place in placesDistance:
+            if distance < radius:
+                location = Location(jsonDict=place)
+                return session.merge(location)
+    if withFallback:
+        return locationFromLatLon(session, latitude, longitude)
+    return None
+
 
 def chargerFromLatLonWithGeofence(weConnect, session, latitude, longitude, searchRadius):
     geofences: Geofence = session.query(Geofence).filter(and_(Geofence.latitude.isnot(None), Geofence.longitude.isnot(None))).all()
     geofenceDistance = [(haversine((latitude, longitude), (geofence.latitude, geofence.longitude), unit=Unit.METERS), geofence) for geofence in geofences]
     geofenceDistance = sorted(geofenceDistance, key=lambda geofence: geofence[0])
-    print('LALALALALA')
-    print(geofenceDistance)
     for distance, geofence in geofenceDistance:
         if distance < (geofence.radius + searchRadius) and geofence.charger is not None:
             return geofence.charger
