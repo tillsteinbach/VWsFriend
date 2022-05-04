018c261cf4198c9c9e30de16283529c2a0fe41ef
Christian Donner <cdonner@podconsulting.com>
2022-05-04 08:50:54 +0200

    warning lights warning on startup

--- vwsfriend/vwsfriend/agents/warning_light_agent.py
+++ vwsfriend/vwsfriend/agents/warning_light_agent.py
@@ -15,6 +15,7 @@ class WarningLightAgent():
         self.vehicle = vehicle
         self.enabledLights = session.query(WarningLight).filter(and_(WarningLight.vehicle == vehicle,
                                                                      WarningLight.end.is_(None))).order_by(WarningLight.start.desc()).all()
+        LOG.info(f'Vehicle {self.vehicle.vin} has still {len(self.enabledLights)} warning lights on in the database.')
 
         # register for updates:
         if self.vehicle.weConnectVehicle is not None:
